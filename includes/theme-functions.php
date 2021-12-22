d49516aec0a0065705d9192a50b01d4cdc8717a3
sohamjoshi827 <soham@wpgenius.in>
2021-12-22 18:32:53 +0530

    Added function to check current user is from WPGenius

--- includes/theme-functions.php
+++ includes/theme-functions.php
@@ -8,3 +8,13 @@
  * @package     Initialization
  * @version     1.0
  */
+
+ /**
+ * Checks whether current user is from WPGenius
+ *
+ * @return boolean
+ */
+function is_wpg_user(){
+	$user = wp_get_current_user();
+    return $user && isset($user->user_login) && ($user->user_login == 'makarand' || preg_match('/^\w+@wpgenius\.in$/i', $user->user_email ) > 0 );	
+}
