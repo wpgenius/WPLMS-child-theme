afa601a703d514803f7bf91bb5a315fa0b1e9b71
sohamjoshi827 <soham@wpgenius.in>
2021-12-22 17:58:55 +0530

    Admin: CSS to print admin/dashboard.

--- includes/admin-actions.php
+++ includes/admin-actions.php
@@ -8,3 +8,20 @@
  * @package     Initialization
  * @version     1.0
  */
+
+/**
+ * CSS to print admin/dashboard
+ * - Hide posts selection page option
+ * - Hide unwanted options from dashbords
+ * 
+ * @return void
+ */
+function cmp_admin_css(){
+	?>
+    <style type="text/css">
+		#front-static-pages .screen-reader-text + p , /* Hide posts selection page option */
+		tr.show-admin-bar, .user-rich-editing-wrap, .user-admin-color-wrap, .yoast.yoast-settings, #fieldset-billing + h2, #fieldset-shipping { display: none !important;	}
+	</style>
+    <?php
+}
+add_action( 'admin_print_styles', 'cmp_admin_css' );
