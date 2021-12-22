c541824d39160892889fa6205834e24948c491e4
sohamjoshi827 <soham@wpgenius.in>
2021-12-22 15:44:13 +0530

    Add settings under WPLMS email settings for Reply to email address. and Add reply to email address in BuddyPress emails.

--- includes/options.php
+++ includes/options.php
@@ -8,3 +8,19 @@
  * @package     Initialization
  * @version     1.0
  */
+
+add_filter('wplms_email_settings', 'cmp_wplms_email_settings', 9999);
+function cmp_wplms_email_settings($settings)
+{
+
+    $new_option = array(
+        array(
+            'label' => __('Reply-To "Email"', 'vibe-customtypes'),
+            'name' => 'reply_to_email',
+            'type' => 'text',
+            'desc' => __('Email to which student should reply on recevied email.', 'vibe-customtypes')
+        )
+    );
+
+    return array_merge(array_slice($settings, 0, 2), $new_option, array_slice($settings, 2));
+}
