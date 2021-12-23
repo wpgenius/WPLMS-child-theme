cc9323bf683a2d41cb313507d83019f8ce86f5b5
sohamjoshi827 <soham@wpgenius.in>
2021-12-23 14:36:03 +0530

    Hide virtual and downloadable option and set virtual type by default.

--- includes/woocommerce-actions.php
+++ includes/woocommerce-actions.php
@@ -8,3 +8,28 @@
  * @package     Initialization
  * @version     1.0
  */
+
+/**
+ * Hide virtual and downloadable option and set virtual type by default.
+ *
+ * @return void
+ */
+function wplms_hide_and_enable_virtual_by_default(){
+    ?>
+    <style>
+        label[for="_virtual"], label[for="_downloadable"]{ opacity: 0; pointer-events: none; }
+    </style>
+    <?php
+    ## JQUERY SCRIPT ##
+    // Here we set as selected the 'virtual' checkboxes by default
+    ?>
+    <script>
+        (function($){
+            $('input[name=_virtual]').prop('checked', true);
+            $('input[name=_downloadable]').prop('checked', false);
+        })(jQuery);
+    </script>
+    <?php
+}
+add_action( 'woocommerce_product_options_general_product_data', 'wplms_hide_and_enable_virtual_by_default' );
+
