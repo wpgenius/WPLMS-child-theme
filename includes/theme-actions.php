61a4b649f7db359bbc3301fa25bf58a16a3996ab
Makarand Mane <makarand@wpgenius.in>
2022-02-24 11:30:37 +0530

    Remove WooCommerce Block styles

--- includes/theme-actions.php
+++ includes/theme-actions.php
@@ -14,12 +14,15 @@
  *
  * @return void
  */
-function remove_wp_block_library_css(){
+function remove_wp_block_library_css()function remove_wp_block_library_css(){
     wp_dequeue_style( 'wp-block-library' );
     wp_dequeue_style( 'bp-member-block' );
     wp_dequeue_style( 'wc-block-style' );
     wp_dequeue_style( 'wp-block-library-theme' );
-   }
+    wp_dequeue_style( 'wc-blocks-vendors-style' );
+	wp_deregister_style( 'wc-block-editor' );
+	wp_deregister_style( 'wc-blocks-style' );
+}
 add_action( 'wp_enqueue_scripts', 'remove_wp_block_library_css' );
 
 /**
